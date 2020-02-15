54ac29f97fec0ad4816ea58d7fe4ba27523c6bc1
marco <marco@DESKTOP-32LTOTK>
2020-02-15 10:30:06 -0500

    JBHunt

---
+++ src/jbhunt/time.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbhunt;
+
+/**
+ *
+ * @author marco
+ */
+public class time {
+   private int time;
+   private int building;
+   private String statement;
+
+    public time() {
+        
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(int building) {
+        this.building = building;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+
+   
+   
+   
+   
+   
+   
+    
+}
